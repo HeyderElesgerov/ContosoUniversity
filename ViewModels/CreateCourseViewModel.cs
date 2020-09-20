378df03bb48003441f5f62dd2c25b5b0eaa7e311
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2020-09-20 20:46:22 +0400

    Created a new viewmodel to use when posting new course.

---
+++ ViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class CreateCourseViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int Credits { get; set; }
+
+        public Department Department { get; set; }
+
+        public List<SelectListItem> DepartmentsSelectList { get; set; }
+    }
+}
