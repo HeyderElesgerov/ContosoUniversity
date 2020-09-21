733a590ed46b25578689ff0065d08a7710f675f7
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2020-09-21 16:14:55 +0400

    Added EditCourseViewModel to use when editing course in a form

---
+++ ViewModels/EditCourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class EditCourseViewModel
+    {
+        public EditCourseViewModel(int id)
+        {
+            this.ID = id;
+        }
+
+        public readonly int ID;
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Credits { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public List<SelectListItem> DepartmentsSelectListItems { get; set; }
+    }
+}
