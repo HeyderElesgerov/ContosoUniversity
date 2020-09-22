ff48c0f5b5e2c39c8c32a67d0017e3fbe7c2e0b8
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2020-09-22 17:14:01 +0400

    Added a from which you can add new Instructor

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -1,8 +1,9 @@
 ﻿using ContosoUniversity.Models;
 using ContosoUniversity.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -38,7 +39,7 @@ namespace ContosoUniversity.Controllers
 
             var selectedInstructor = instructors.Where(i => i.ID == selectedInstructorId).FirstOrDefault();
 
-            if(selectedInstructor != null)
+            if (selectedInstructor != null)
             {
                 viewModel.CoursesOfSelectedInstructor = from courseAssignment in selectedInstructor.CourseAssignments
                                                         select courseAssignment.Course;
@@ -61,6 +62,91 @@ namespace ContosoUniversity.Controllers
             }
 
             return View(viewModel);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var viewModel = new NewInstructorViewModel();
+            viewModel.CourseAssignments = await PopulateCourseAssignments();
+
+            return View(viewModel);
+        }
+        //post
+        [HttpPost]
+        public async Task<IActionResult> Create(NewInstructorViewModel newInstructorViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var newInstructor = new Instructor();
+                newInstructor.FirstName = newInstructorViewModel.FirstName;
+                newInstructor.LastName = newInstructorViewModel.LastName;
+                newInstructor.HireDate = newInstructor.HireDate;
+
+                if (string.IsNullOrEmpty(newInstructorViewModel.OfficeLocation))
+                {
+                    newInstructor.OfficeAssignment = null;
+                }
+                else
+                {
+                    newInstructor.OfficeAssignment = new OfficeAssignment()
+                    {
+                        Location = newInstructorViewModel.OfficeLocation,
+                        Instructor = newInstructor
+                    };
+                }
+                _context.Instructors.Add(newInstructor);
+
+                foreach (var courseAssignment in newInstructorViewModel.CourseAssignments)
+                {
+                    if (courseAssignment.IsAssigned)
+                    {
+                        var courseInDb = await _context.Courses.Where(c => c.ID == courseAssignment.CourseID).FirstOrDefaultAsync();
+
+                        if (courseInDb != null)
+                        {
+                            _context.CourseAssignments.Add(
+                                new CourseAssignment()
+                                {
+                                    Course = courseInDb,
+                                    Instructor = newInstructor
+                                });
+                        }
+                    }
+                }
+
+                try
+                {
+
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DBConcurrencyException)
+                {
+                    ModelState.AddModelError("Database Error", "Unable to insert new record. Try again later.");
+                }
+            }
+
+            newInstructorViewModel.CourseAssignments = await PopulateCourseAssignments();
+            return View(newInstructorViewModel);
+        }
+
+        private async Task<List<CourseAssignmentViewModel>> PopulateCourseAssignments()
+        {
+            var courseAssignments = new List<CourseAssignmentViewModel>();
+
+            foreach (var course in await _context.Courses.ToListAsync())
+            {
+                var courseAssignmentViewModel = new CourseAssignmentViewModel
+                {
+                    CourseID = course.ID,
+                    CourseTitle = course.Title,
+                    IsAssigned = false
+                };
+
+                courseAssignments.Add(courseAssignmentViewModel);
+            }
+
+            return courseAssignments;
         }
     }
 }
---
+++ ViewModels/CourseAssignmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class CourseAssignmentViewModel
+    {
+        public int CourseID { get; set; }
+        public string CourseTitle { get; set; }
+        public bool IsAssigned { get; set; }
+    }
+}
