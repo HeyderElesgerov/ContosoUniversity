72581af649375c002eb299565f9772c8288e9b0e
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2020-09-25 21:21:04 +0400

    Added Departments page

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private SchoolContext _context;
+
+        public DepartmentController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _context.Departments
+                                    .Include(p => p.Adminstrator).ToListAsync();
+            return View(departments);
+        }
+    }
+}
