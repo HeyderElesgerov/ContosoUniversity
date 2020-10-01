68734ff3196f7aa7ae6f3373fde19b8437bbc617
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2020-10-01 15:41:32 +0400

    Added Person class to create inheritance with Instructor and Student classes.

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(maximumLength: 30, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(maximumLength: 50, MinimumLength = 2)]
+        public string LastName { get; set; }
+    }
+}
