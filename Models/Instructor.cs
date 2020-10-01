fc2f4fe6aae626a27b2b6b5dd71165b45eb45457
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2020-10-01 15:43:43 +0400

    Created inheritance between Person-Student and Person-Instructor

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -6,20 +6,8 @@ using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
-    public class Instructor
+    public class Instructor : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "First Name")]
-        [StringLength(maximumLength: 30, MinimumLength = 2)]
-        public string FirstName { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(maximumLength: 50, MinimumLength = 2)]
-        public string LastName { get; set; }
-
         [DataType(DataType.Date)]
         [Required]
         [Display(Name = "Hire Date")]
@@ -27,6 +15,8 @@ namespace ContosoUniversity.Models
 
         [Display(Name = "Office")]
         public OfficeAssignment OfficeAssignment { get; set; }
+
+
         public ICollection<Department> Departments { get; set; }
 
         [Display(Name = "Courses")]
--- Models/Student.cs
+++ Models/Student.cs
@@ -4,20 +4,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Display(Name = "First Name")]
-        [Required]
-        [StringLength(30)]
-        public string FirstName { get; set; }
-
-        [Display(Name = "Last Name")]
-        [Required]
-        [StringLength(50)]
-        public string LastName { get; set; }
-
         [DataType(DataType.Date)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
